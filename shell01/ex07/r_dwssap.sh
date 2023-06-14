79a07e3a33027bbaf4617d5669bcd28d4cddaf27
Alix THIEBAUT <athiebau@paul-f3Ar5s5.clusters.42paris.fr>
2023-06-14 15:53:31 +0200

    oui

--- shell01/ex07/r_dwssap.sh
+++
@@ -1,2 +0,0 @@
-awk -F ":" '{ $2 = "" ; print $1 }' /etc/passwd | cut -d " " -f1 | awk 'NR%2==0' | rev | sort -r | awk "NR>=$FT_LINE1 && NR<=$FT_LINE2" | sed 's/$/,/g' | tr "\n" " " | sed 's/..$/./'
-
